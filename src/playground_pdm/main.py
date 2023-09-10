27c526c60b2141e51864239c63a2d3e3b7f43015
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-11 00:28:49 +0900

    Update:

--- src/playground_pdm/main.py
+++ src/playground_pdm/main.py
@@ -1,3 +1,8 @@
 import toml
+import os
 
-print(toml.load("./config.toml"))
+basename: str = os.path.basename(__file__)
+dirname: str = os.path.abspath(os.path.dirname(__file__))
+print(basename)
+print(dirname)
+print(toml.load(os.path.join(dirname, 'config.toml')))
