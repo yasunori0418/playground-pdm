833a6b19768da4493154c51c1ec7d653f92572da
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-15 00:23:12 +0900

    Update: デコレータでloguru

--- src/playground_pdm/__main__.py
+++ src/playground_pdm/__main__.py
@@ -1,8 +1,29 @@
-import toml
-import os
+from loguru import logger
+from functools import wraps
+import sys
 
-basename: str = os.path.basename(__file__)
-dirname: str = os.path.abspath(os.path.dirname(__file__))
-print(basename)
-print(dirname)
-print(toml.load(os.path.join(dirname, 'config.toml')))
+def log():
+    def log_wrapper(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            logger.remove()
+            log_format: str = '<level>{level}</level> | <cyan>{extra[func]}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>'
+            func_name = func.__name__
+            custom_logger = logger.bind(func=func.__name__)
+            custom_logger.add(sys.stdout, format=log_format)
+            custom_logger.debug('start')
+            custom_logger.info(f'function name: {func_name}')
+            with custom_logger.catch(level="CRITICAL", message="exception test message"):
+                result = func(*args, **kwargs)
+            custom_logger.debug('end')
+            return result
+        return wrap
+    return log_wrapper
+
+@log()
+def test_deco():
+    print("デコレーターテスト")
+    raise Exception("error test")
+
+if __name__ == '__main__':
+    test_deco()
